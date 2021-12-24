d340988d21644b3b415d4aaeb013940b6c2ba5c9
nav-ya-singh <95939683+nav-ya-singh@users.noreply.github.com>
2021-12-24 00:51:02 -0600

    Add files via upload

---
+++ parse_download.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+import argparse
+import requests
+
+parser = argparse.ArgumentParser()
+parser.add_argument("filename", type=argparse.FileType('r'))
+args = parser.parse_args()
+lines_list = []
+for line in args.filename:
+    stripped_line = line.strip()
+    lines_list.append(stripped_line)
+print(lines_list)
+
+print("start download")
+for pdb_id in lines_list:
+    url = "https://files.rcsb.org/download/" + pdb_id + ".cif.gz"
+    r = requests.get(url)
+    filename = url.split('/')[-1]
+    with open(filename, 'wb') as cif_file:
+        cif_file.write(r.content)
+print('download_complete')
